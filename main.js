9726d8349f1cca38b8ae5d8e1b93d7757c97d2b5
Victor Martins dos Reis <vitor_jhoul@hotmail.com>
2021-06-21 23:40:26 -0300

    Criação do JS
    Criação do JavaScript e continuação da Criação da estrutura base e layout via CSS

---
+++ main.js
@@ -0,0 +1,33 @@
+/* Abre e fecha o menu ao clicar no hamburguer e X */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/*Esconder o  menu ao clicar em um item do menu */
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* Colocar Sombra no Header quando o usuario rolar  o Scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
+  }
+})
